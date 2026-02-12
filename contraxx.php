46fcec9434e333bb3b4c021505691405e5843d92
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2026-02-11 23:15:58 -0500

    Opciones - Cambio de contraseña

--- contraxx.php
+++ contraxx.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
-* @author Cesar Gonzalez <aurigadl@gmail.com>
-* @author Jairo Losada   <jlosada@gmail.com>
-* @license  GNU AFFERO GENERAL PUBLIC LICENSE
-* @copyright
+ * @author Cesar Gonzalez <aurigadl@gmail.com>
+ * @author Jairo Losada   <jlosada@gmail.com>
+ * @license  GNU AFFERO GENERAL PUBLIC LICENSE
+ * @copyright
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -18,17 +18,21 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 session_start();
 
 $ruta_raiz = ".";
-if (!$_SESSION['dependencia'])
-    header ("Location: $ruta_raiz/cerrar_session.php");
+if (!$_SESSION['dependencia']) {
+    header("Location: $ruta_raiz/cerrar_session.php");
+}
 
-foreach ($_GET as $key => $valor)   ${$key} = $valor;
-foreach ($_POST as $key => $valor)   ${$key} = $valor;
-
+foreach ($_GET as $key => $valor) {
+    ${$key} = $valor;
+}
+foreach ($_POST as $key => $valor) {
+    ${$key} = $valor;
+}
 
 $krd         = $_SESSION["krd"];
 $dependencia = $_SESSION["dependencia"];
@@ -42,11 +46,11 @@ include_once "$ruta_raiz/include/db/ConnectionHandler.php";
 $db = new ConnectionHandler($ruta_raiz);
 
 $numeroa =
-$numero  =
-$numeros =
-$numerot =
-$numerop =
-$numeroh =0;
+    $numero  =
+    $numeros =
+    $numerot =
+    $numerop =
+    $numeroh = 0;
 
 $isql = "select
               a.*
@@ -66,135 +70,131 @@ $usua_login      = $rs->fields["USUA_LOGIN"];
 
 ?>
 <html>
-    <head>
-        <title>Cambio de Contrase&ntilde;as</title>
-        <?php include_once "$ruta_raiz/htmlheader.inc.php"; ?>
-        <style>
-           .error {
-             color: red;
-             margin-left: 5px;
-           }
 
-           label.error {
-             display: inline;
-           }
-        </style>
-    </head>
+<head>
+    <title>Cambio de Contrase&ntilde;as</title>
+    <?php include_once "$ruta_raiz/htmlheader.inc.php"; ?>
+    <style>
+        .error {
+            color: red;
+            margin-left: 5px;
+        }
 
-    <body>
-        <form action='usuarionuevo.php' method="post" id="loginform" class="form-horizontal" role="form">
-            <input type='hidden' name='<?=session_name()?>' value='<?=session_id()?>'>
-            <div class="col-sm-12">
-                <!-- widget grid -->
-                <section id="widget-grid">
-                    <!-- row -->
-                    <div class="row">
-                      <!-- NEW WIDGET START -->
-                      <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <!-- Widget ID (each widget will need unique ID)-->
-                        <div class="jarviswidget jarviswidget-color-darken" id="wid-id-1" data-widget-editbutton="false">
-                          <header>
-                            <h2>
-                              Cambio de contrase&ntilde;a<br>
-                              <small><?=$tituloCrear ?></small>
-                            </h2>
-                          </header>
-                          <!-- widget content -->
-                          <div class="widget-body">
-                              <div style="padding-top:30px" class="panel-body" >
+        label.error {
+            display: inline;
+        }
+    </style>
+</head>
 
-                                      <label for="contradrd">Contraseña:</label>
-                                      <div class="input-group">
-                                          <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                          <input
-                                              type="password"
-                                              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}"
-                                              name="contradrd"
-                                              id="contradrd"
-                                              placeholder="Contraseña"
-                                              class="form-control"
-                                              required>
-                                      </div>
-                                      <p id="emailHelp" class="form-text text-muted">
-                                          La contraseña debe tener mas de 7
-                                          caracteres y debe contener numeros,
-                                          letras en minusculas y mayusculas.
-                                      </p>
+<body>
+    <form action='usuarionuevo.php' method="post" id="loginform" class="needs-validation" role="form">
+        <input type='hidden' name='<?= session_name() ?>' value='<?= session_id() ?>'>
 
-                                      <label for="contraver">Confirmar Contraseña:</label>
-                                      <div style="margin-bottom: 25px" class="input-group">
-                                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                         <input
-                                             type="password"
-                                             pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
-                                             name="contraver"
-                                             id="contraver"
-                                             class="form-control"
-                                             placeholder="Repetir Contraseña"
-                                             required>
-                                      </div>
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6 col-lg-5">
+                    <div class="card shadow-lg border-0">
+                        <div class="card-header bg-dark bg-gradient text-white py-3">
+                            <h5 class="mb-0 fw-bold">
+                                <i class="fa fa-key me-2 text-warning"></i> Cambio de contraseña
+                            </h5>
+                            <?php if (!empty($tituloCrear)): ?>
+                                <small class="text-white-50"><?= $tituloCrear ?></small>
+                            <?php endif; ?>
+                        </div>
+                        <div class="card-body p-4">
+                            <div class="mb-3">
+                                <label for="contradrd" class="form-label fw-semibold">Contraseña Nueva:</label>
+                                <div class="input-group">
+                                    <span class="input-group-text bg-light"><i class="fa fa-lock"></i></span>
+                                    <input
+                                        type="password"
+                                        name="contradrd"
+                                        id="contradrd"
+                                        class="form-control"
+                                        placeholder="Ingrese su nueva clave"
+                                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}"
+                                        required>
+                                </div>
+                                <div id="passwordHelp" class="form-text mt-2 small lh-sm">
+                                    <i class="fa fa-info-circle me-1"></i> Requisitos: Mínimo 7 caracteres, incluyendo números, letras minúsculas y mayúsculas.
+                                </div>
+                            </div>
+                            <div class="mb-4">
+                                <label for="contraver" class="form-label fw-semibold">Confirmar Contraseña:</label>
+                                <div class="input-group">
+                                    <span class="input-group-text bg-light"><i class="fa fa-check-circle"></i></span>
+                                    <input
+                                        type="password"
+                                        name="contraver"
+                                        id="contraver"
+                                        class="form-control"
+                                        placeholder="Repita la clave"
+                                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+                                        required>
+                                </div>
+                            </div>
 
-                                      <div style="margin-top:10px" class="form-group">
-                                          <!-- Button -->
-                                          <div class="col-sm-12 controls">
-                                            <input
-                                                class="btn btn-success"
-                                                type="submit"
-                                                value="Aceptar">
-                                          </div>
-                                      </div>
-                              </div>
-                      </article>
-                </section>
+                            <div class="d-grid gap-2 mt-2">
+                                <button type="submit" class="btn btn-success btn-lg fw-bold shadow-sm">
+                                    <i class="fa fa-save me-2"></i>Aceptar y Cambiar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </form>
-        <script>
-            jQuery.validator.addMethod( 'passwordMatch', function(value, element) {
-                // The two password inputs
-                var password = $("#contradrd").val();
-                var confirmPassword = $("#contraver").val();
+        </div>
+    </form>
 
-                // Check for equality with the password inputs
-                if (password != confirmPassword) {
-                    return false;
-                } else {
-                    return true;
-                }
+    <script>
+        jQuery.validator.addMethod('passwordMatch', function(value, element) {
+            // The two password inputs
+            var password = $("#contradrd").val();
+            var confirmPassword = $("#contraver").val();
 
-            }, "No son iguales las constraseñas");
+            // Check for equality with the password inputs
+            if (password != confirmPassword) {
+                return false;
+            } else {
+                return true;
+            }
 
-            $("#loginform").validate({
+        }, "No son iguales las constraseñas");
 
-                submitHandler: function(form) {
-                  form.submit();
+        $("#loginform").validate({
+
+            submitHandler: function(form) {
+                form.submit();
+            },
+
+            rules: {
+                contradrd: {
+                    required: true,
+                    minlength: 7,
                 },
 
-                rules: {
-                    contradrd: {
-                        required: true,
-                        minlength: 7,
-                    } ,
-
-                    contraver: {
-                        equalTo: "#contradrd",
-                        minlength: 7,
-                        passwordMatch: true
-                    }
-                },
-
-                // messages
-                messages: {
-                    contradrd: {
-                        required: "El campo contraseña es requerido",
-                        minlength: "El campo contraseña requiere mas de 7 caracteres",
-                    },
-                    contraver: {
-                        required: "El campo confimar contraseña es requerido",
-                        minlength: "El campo contraseña requiere mas de 7 caracteres",
-                        passwordMatch: "Las constraseñas no son iguales", // custom message for mismatched passwords
-                    }
+                contraver: {
+                    equalTo: "#contradrd",
+                    minlength: 7,
+                    passwordMatch: true
                 }
-            });
-        </script>
-    </body>
+            },
+
+            // messages
+            messages: {
+                contradrd: {
+                    required: "El campo contraseña es requerido",
+                    minlength: "El campo contraseña requiere mas de 7 caracteres",
+                },
+                contraver: {
+                    required: "El campo confimar contraseña es requerido",
+                    minlength: "El campo contraseña requiere mas de 7 caracteres",
+                    passwordMatch: "Las constraseñas no son iguales", // custom message for mismatched passwords
+                }
+            }
+        });
+    </script>
+</body>
+
 </html>
