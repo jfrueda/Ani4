687cb4c1131bcd1ba28079fc58226e4ae467f0e5
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2026-02-24 17:07:42 -0500

    combos validados

--- js/crea_combos_2.js
+++ js/crea_combos_2.js
@@ -22,22 +22,75 @@ function cambia(parametros) {
 	var id2mod = cambia.arguments[1];
 	var idpadre = cambia.arguments[2];
 
-	oCntrl = document.getElementById(id2mod);
-	if (oCntrl.type == 'select-one') {
-		for (var i = 0; i < forma.elements.length; i++) {	// Realizamos procesos y creamos variables respecto al combo a modificar.
-			if (forma.elements[i].name == id2mod) {	// Capturamos el objeto a modificar y su posicion en el formulario.
-				oCntrl = forma.elements[i];
-				pos = i;
-				prefijo = (oCntrl.name).substring(0, (oCntrl.name).length - 1);
-				if (prefijo == 'muni_us') {	// En caso de ser el combo municipio a modificar, capturamos la opcion del combo pais como referencia
-					// adicional. Se asume que el objeto pais se encuentra a 2 posiciones del combo municipios.
-					$idpas = forma.elements[i - 2].options[forma.elements[i - 2].selectedIndex].value;
-					break;
+	var oCntrl = document.getElementById(id2mod);
+
+	// 1. Validar que el control existe antes de acceder
+	if (oCntrl && oCntrl.type === 'select-one') {
+		if (forma && forma.elements) {
+			for (var i = 0; i < forma.elements.length; i++) {	// Realizamos procesos y creamos variables respecto al combo a modificar.
+				// if (forma.elements[i].name == id2mod) {	// Capturamos el objeto a modificar y su posicion en el formulario.
+				// 	oCntrl = forma.elements[i];
+				// 	pos = i;
+				// 	prefijo = (oCntrl.name).substring(0, (oCntrl.name).length - 1);
+				// 	if (prefijo == 'muni_us') {	// En caso de ser el combo municipio a modificar, capturamos la opcion del combo pais como referencia
+				// 		// adicional. Se asume que el objeto pais se encuentra a 2 posiciones del combo municipios.
+				// 		$idpas = forma.elements[i - 2].options[forma.elements[i - 2].selectedIndex].value;
+				// 		break;
+				// 	}
+				// }
+
+				var elementoActual = forma.elements[i];
+
+				// 2. Validar existencia del elemento en la iteración
+				if (elementoActual && elementoActual.name === id2mod) {
+					oCntrl = elementoActual;
+					pos = i;
+
+					// Usamos validación de string para el nombre
+					var nombreControl = oCntrl.name || "";
+					prefijo = nombreControl.substring(0, nombreControl.length - 1);
+
+					if (prefijo === 'muni_us') {
+						// 3. VALIDACIÓN : i - 2
+						// Verificamos que el índice sea mayor o igual a 0
+						// Verificamos que el elemento en i-2 exista y tenga opciones
+						var elementoPais = (i >= 2) ? forma.elements[i - 2] : null;
+
+						if (elementoPais && elementoPais.options && elementoPais.selectedIndex >= 0) {
+							// Capturamos el valor de forma segura
+							$idpas = elementoPais.options[elementoPais.selectedIndex].value;
+						} else {
+							console.warn("No se pudo acceder al combo país en la posición " + (i - 2));
+							$idpas = null;
+						}
+						break;
+					}
 				}
-			}
-			// Creamos variable respecto al combo padre.
-			if (forma.elements[i].name == idpadre) {	// Capturamos la opci�n del combo seleccionada en el combo "padre".
-				var opc_padre = forma.elements[i].options[forma.elements[i].selectedIndex].value;
+				
+				// Creamos variable respecto al combo padre.
+				// if (forma?.elements[i]?.name == idpadre) {	// Capturamos la opci�n del combo seleccionada en el combo "padre".
+				// 	var opc_padre = forma?.elements[i]?.options[forma?.elements[i]?.selectedIndex]?.value;
+				// }
+
+				// Validamos que 'forma' y 'forma.elements' existan antes de entrar al ciclo o lógica
+				if (forma && forma.elements) {
+					const elementoActual = forma.elements[i];
+					if (elementoActual && elementoActual.name === idpadre) {
+						// Validamos la existencia de options y que el selectedIndex sea válido (mayor a -1)
+						const tieneOpciones = elementoActual.options && elementoActual.options.length > 0;
+						const indiceValido = elementoActual.selectedIndex >= 0;
+
+						if (tieneOpciones && indiceValido) {
+							// Ahora es 100% seguro acceder al value
+							var opc_padre = elementoActual.options[elementoActual.selectedIndex].value;
+
+							console.log("Opción padre capturada:", opc_padre);
+						} else {
+							console.warn("El combo padre existe pero no tiene opciones o selección válida.");
+							var opc_padre = null; // O un valor por defecto
+						}
+					}
+				}
 			}
 		}
 		Vector = new Array();
