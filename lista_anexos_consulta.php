729e5ae324f5acbc870ce281aa97ea46b29556ba
daniel feo <danielandres.feo@gmail.com>
2026-03-18 21:28:32 -0500

    fix: cambio logo en consulta anexos

--- lista_anexos_consulta.php
+++ lista_anexos_consulta.php
@@ -338,7 +338,7 @@ $tieneAsignacion = true;
         <fieldset>
             <div CLASS="form-pqrs-contInput" >
                 <div CLASS="form-pqrs-contImg">
-                    <img alt="Super Salud" src="./bodega/sys_img/logosupersalud.png" longdesc="http://www.example.com/description.txt"></img>
+                    <img alt="Universidad Militar Nueva Granada" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Escudo_oficial_Universidad_Militar_Nueva_Granada.svg/960px-Escudo_oficial_Universidad_Militar_Nueva_Granada.svg.png"></img>
                 </div>
                 <br>
                 <br>
@@ -1108,4 +1108,3 @@ $tieneAsignacion = true;
     }
 
 </script>
-
