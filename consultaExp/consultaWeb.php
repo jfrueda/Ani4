fdf808b5b0ea83d75c08ee0e33da6c0112bc5f8b
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2026-04-28 10:57:14 -0500

    Consulta Exp - Update PHP V1.0

--- consultaExp/consultaWeb.php
+++ consultaExp/consultaWeb.php
@@ -1,69 +1,67 @@
 <html>
-<head><TITLE>Consulta Web - ORFEO - Sistema de Gestion Documental</TITLE>
-<script>
-function loginTrue()
-{
-	document.formulario.submit();
-}
-</script>
+
+<head>
+	<TITLE>Consulta Web - ORFEO - Sistema de Gestion Documental</TITLE>
+	<script>
+		function loginTrue() {
+			document.formulario.submit();
+		}
+	</script>
 </head>
 <!--
 <body bgcolor="#003399" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 -->
 <center>
 
-<?php
-define('ADODB_ASSOC_CASE', 1);
-/** FORMULARIO DE LOGIN A ORFEO
-  * Aqui se inicia session 
-	* @PHPSESID		String	Guarda la session del usuario
-	* @db 					Objeto  Objeto que guarda la conexion Abierta.
-	* @iTpRad				int		Numero de tipos de Radicacion
-	* @$tpNumRad	array 	Arreglo que almacena los numeros de tipos de radicacion Existentes
-	* @$tpDescRad	array 	Arreglo que almacena la descripcion de tipos de radicacion Existentes
-	* @$tpImgRad	array 	Arreglo que almacena los iconos de tipos de radicacion Existentes
-	* @query				String	Consulta SQL a ejecutar
-	* @rs					Objeto	Almacena Cursor con Consulta realizada.
-	* @numRegs		int		Numero de registros de una consulta
-	*/
-$ruta_raiz = "..";
-include_once "$ruta_raiz/include/db/ConnectionHandler.php";
-$db = new ConnectionHandler($ruta_raiz);
-$db->conn->SetFetchMode(ADODB_FETCH_ASSOC);
-$fechah = date("dmy") . "_" . time("hms");
-$usua_nuevo=3;
-if ($numeroRadicado)
-	{
-		$numeroRadicado = str_replace("-","",$numeroRadicado);
-		$numeroRadicado = str_replace("_","",$numeroRadicado);
-		$numeroRadicado = str_replace(".","",$numeroRadicado);
-		$numeroRadicado = str_replace(",","",$numeroRadicado);
-		$numeroRadicado = str_replace(" ","",$numeroRadicado);
-		include "$ruta_raiz/include/tx/ConsultaRad.php";	
+	<?php
+	define('ADODB_ASSOC_CASE', 1);
+	/** FORMULARIO DE LOGIN A ORFEO
+	 * Aqui se inicia session 
+	 * @PHPSESID		String	Guarda la session del usuario
+	 * @db 					Objeto  Objeto que guarda la conexion Abierta.
+	 * @iTpRad				int		Numero de tipos de Radicacion
+	 * @$tpNumRad	array 	Arreglo que almacena los numeros de tipos de radicacion Existentes
+	 * @$tpDescRad	array 	Arreglo que almacena la descripcion de tipos de radicacion Existentes
+	 * @$tpImgRad	array 	Arreglo que almacena los iconos de tipos de radicacion Existentes
+	 * @query				String	Consulta SQL a ejecutar
+	 * @rs					Objeto	Almacena Cursor con Consulta realizada.
+	 * @numRegs		int		Numero de registros de una consulta
+	 */
+	$ruta_raiz = "..";
+	include_once "$ruta_raiz/include/db/ConnectionHandler.php";
+	$db = new ConnectionHandler($ruta_raiz);
+	$db->conn->SetFetchMode(ADODB_FETCH_ASSOC);
+	$fechah = date("dmy") . "_" . time("hms");
+	$usua_nuevo = 3;
+	if ($numeroRadicado) {
+		$numeroRadicado = str_replace("-", "", $numeroRadicado);
+		$numeroRadicado = str_replace("_", "", $numeroRadicado);
+		$numeroRadicado = str_replace(".", "", $numeroRadicado);
+		$numeroRadicado = str_replace(",", "", $numeroRadicado);
+		$numeroRadicado = str_replace(" ", "", $numeroRadicado);
+		include "$ruta_raiz/include/tx/ConsultaRad.php";
 		$ConsultaRad = new ConsultaRad($db);
 		$idWeb = $ConsultaRad->idRadicado($numeroRadicado);
-		if($numeroRadicado==$idWeb and substr($numeroRadicado,-1)=='2')
-		{
-			$ValidacionWeb="Si";
+		if ($numeroRadicado == $idWeb and substr($numeroRadicado, -1) == '2') {
+			$ValidacionWeb = "Si";
 			$idRadicado = $idWeb;
-		}
-		else
-		{
-			$ValidacionWeb="No";
+		} else {
+			$ValidacionWeb = "No";
 			$mensaje = "El numero de radicado digitado no existe o esta mal escrito.  Por favor corrijalo e intente de nuevo.";
 			echo "<center><font color=red class=tpar><font color=red size=3>$mensaje</font></font>";
 			echo "<script>alert('$mensaje');</script>";
-		} 
+		}
 	}
 	$krd = strtoupper($krd);
-	$datosEnvio = "$fechah&".session_name()."=".trim(session_id())."&krd=$krd";
-  ?>
-<head>
-<title>.:: ORFEO, M&oacute;dulo de validaci&oacute;n::.</title>
-<link href="../estilos_totales.css" rel="stylesheet" type="text/css">
-<link rel="shortcut icon" href="imagenes/arpa.gif">
-<script language="JavaScript" type="text/JavaScript">
-<!--
+	$datosEnvio = "$fechah&" . session_name() . "=" . trim(session_id()) . "&krd=$krd";
+	?>
+
+	<head>
+		<title>.:: ORFEO, M&oacute;dulo de validaci&oacute;n::.</title>
+		<link href="../estilos_totales.css" rel="stylesheet" type="text/css">
+		<link rel="shortcut icon" href="imagenes/arpa.gif">
+		<script language="JavaScript" type="text/JavaScript">
+			<!--
 function MM_findObj(n, d) { //v4.01
   var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
     d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
@@ -88,21 +86,22 @@ function MM_validateForm() { //v4.0
   document.MM_returnValue = (errors == '');
 }
 //-->
-</script>
-<script>
-function loginTrue()
-{
-	document.formulario.submit();
-}
-</script>
-</head>
-<body  valign=center>	
+		</script>
+		<script>
+			function loginTrue() {
+				document.formulario.submit();
+			}
+		</script>
+	</head>
 
-<form action="consultaWeb.php?fechah=<?=$fechah?>" method="post" onSubmit="MM_validateForm('krd','','R','drd','','R');return document.MM_returnValue" name="form33">
-<?
- include "consulta.php";
-?>
+	<body valign=center>
 
-</form>
-</body>
+		<form action="consultaWeb.php?fechah=<?= $fechah ?>" method="post" onSubmit="MM_validateForm('krd','','R','drd','','R');return document.MM_returnValue" name="form33">
+			<?
+			include "consulta.php";
+			?>
+
+		</form>
+	</body>
+
 </html>
