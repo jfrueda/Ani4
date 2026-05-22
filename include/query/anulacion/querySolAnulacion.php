45f935f1a662e5ef698f635204943784384fd482
daniel feo <dfeo@ucundinamarca.edu.co>
2026-05-22 11:43:41 -0500

    fix: anulacion expedientes

--- include/query/anulacion/querySolAnulacion.php
+++ include/query/anulacion/querySolAnulacion.php
@@ -43,22 +43,22 @@
 						order by '.$order .' ' .$orderTipo;
 			break;	
 	//Modificado skina
-	default: 
-		$isql = 'select
-				b.RADI_NUME_RADI 	as "IMG_Numero Radicado"
-				,b.RADI_PATH 		as "HID_RADI_PATH"
-				,b.RADI_NUME_DERI 	as "Radicado Padre"
-				,b.RADI_FECH_RADI 	as "HOR_RAD_FECH_RADI"
-				,'.$sqlFecha.' 		as "Fecha Radicado"
-				,b.RA_ASUN 		as "Descripcion"
-				,c.SGD_TPR_DESCRIP 	as "Tipo Documento"
-				,b.RADI_NUME_RADI 	as "CHK_CHKANULAR"
-			 from
-				 radicado b,
-				 SGD_TPR_TPDCUMENTO c
-			 where 
-				b.tdoc_codi=c.sgd_tpr_codigo
-				'.$whereFiltro.'
-			order by '.$order .' ' .$orderTipo;	
-	}
-?>
+		default: 
+			$isql = 'select
+					b.RADI_NUME_RADI 	as "IMG_Numero Radicado"
+					,b.RADI_PATH 		as "HID_RADI_PATH"
+					,b.RADI_NUME_DERI 	as "Radicado Padre"
+					,b.RADI_FECH_RADI 	as "HOR_RAD_FECH_RADI"
+					,'.$sqlFecha.' 		as "Fecha Radicado"
+					,b.RA_ASUN 		as "Descripcion"
+					,coalesce(c.SGD_TPR_DESCRIP, \'SIN TIPO DOCUMENTAL\') as "Tipo Documento"
+					,b.RADI_NUME_RADI 	as "CHK_CHKANULAR"
+				 from
+					 radicado b
+					 left join SGD_TPR_TPDCUMENTO c on b.tdoc_codi=c.sgd_tpr_codigo
+				 where 
+					1=1
+					'.$whereFiltro.'
+				order by '.$order .' ' .$orderTipo;	
+		}
+	?>
